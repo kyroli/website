f766b1e39bf4abf302c8fca12de67cf39bbeb79e
kyroli <79038570+kyroli@users.noreply.github.com>
2025-05-30 01:25:24 +0800

    Add files via upload

---
+++ src/components/TheFooter/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'TheFooter',
+})
+</script>
+
+<template>
+  <div mt-44 flex-center gap-x-8>
+
+  </div>
+</template>
---
+++ src/utils/search-engine/index.js
@@ -0,0 +1,24 @@
+import engBaidu from './eng-baidu'
+import engGoogle from './eng-google'
+import engBing from './eng-bing'
+import engWikipedia from './eng-wikipedia'
+
+const ENGINES = {
+  Baidu: engBaidu,
+  Google: engGoogle,
+  Bing: engBing,
+  Wikipedia: engWikipedia,
+}
+
+function target(eng, wd) {
+  return ENGINES[eng].target(wd);
+}
+
+function complete(eng, wd, callback) {
+  return ENGINES[eng].complete(wd, callback);
+}
+
+export default {
+  target,
+  complete,
+}

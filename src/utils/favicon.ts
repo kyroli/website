728aacc142d31d4a95a465a45782501db210ccfc
kyroli <79038570+kyroli@users.noreply.github.com>
2025-11-07 19:38:41 +0800

    Update favicon.ts

--- src/utils/favicon.ts
+++ src/utils/favicon.ts
@@ -1,8 +1,5 @@
 const FAVICON_API = 'https://favicons.fuzqing.workers.dev/api/getFavicon?url='
 
-export const DEFAULT_FAVICON =
-  'data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjMjg5M2ZkIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiI+PHBhdGggZD0iTTE2IDJhMTQgMTQgMCAxIDAgMCAyOGExNCAxNCAwIDAgMCAwLTI4em0wIDIuN2M0LjA3IDAgNy40NyAyLjMyIDkuMjMgNS43aC0xOC40NkM4LjUzIDcuMDIgMTEuOTMgNC43IDE2IDQuN3ptLTkuNjUgNy4zaDE5LjNjLjMyIDEuMDguNDggMi4yLjQ4IDMuM3MtLjE2IDIuMjItLjQ4IDMuM0g2LjM1YTUuODkgNS44OSAwIDAgMSAwLTYuNnptLjkyIDguM2gxNy41Yy0xLjc2IDMuMzgtNS4xNiA1LjctOS4yMyA1LjctNC4wNyAwLTcuNDctMi4zMi05LjIzLTUuN3oiLz48L3N2Zz4='
-
 function getDomainName(url: string) {
   const domain = url.replace(/(^\w+:|^)\/\//, '')
   const matches = domain.match(/([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)/)
@@ -12,7 +9,5 @@ function getDomainName(url: string) {
 export function getFaviconUrl(url: string) {
   const domain = getDomainName(url)
   if (!domain) return ''
-  const faviconUrl = `${FAVICON_API}${domain}&size=64`
-  console.log('[favicon]', faviconUrl)
-  return faviconUrl
+  return `${FAVICON_API}${domain}&size=64`
 }

f5a991461f6c2df34f6487c8756874af884909d4
kyroli <79038570+kyroli@users.noreply.github.com>
2025-11-07 19:28:07 +0800

    Update favicon.ts

--- src/utils/favicon.ts
+++ src/utils/favicon.ts
@@ -1,36 +1,18 @@
 const FAVICON_API = 'https://favicons.fuzqing.workers.dev/api/getFavicon?url='
 
-export const DEFAULT_FAVICON = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIiB2aWV3Qm94PSIwIDAgMzIgMzIiPjxwYXRoIGQ9Ik0xNyAyMmgydjJoLTJ6IiBmaWxsPSJjdXJyZW50Q29sb3IiPjwvcGF0aD48cGF0aCBkPSJNMTcgMThoMnYyaC0yeiIgZmlsbD0iY3VycmVudENvbG9yIj48L3BhdGg+PHBhdGggZD0iTTE0IDI0aDN2MmgtM3oiIGZpbGw9ImN1cnJlbnRDb2xvciI+PC9wYXRoPjxwYXRoIGQ9Ik0xNiAyYTE0IDE0IDAgMSAwIDE0IDE0QTE0LjAxNiAxNC4wMTYgMCAwIDAgMTYgMnpNNC43IDIwSDZsNC4xNzcgMi42YTEgMSAwIDAgMCAxLjM2LS4yOTRsMS4wMDgtMS41MTJhMSAxIDAgMCAwLS4xMjUtMS4yNjFMMTAgMTcuMTEyTDExIDE0aDUuMzIzYTEgMSAwIDAgMCAuOTI4LS42MjhsMS41ODItMy45NTVhMSAxIDAgMCAwLS4wMzQtLjgxOWwtMi4yODYtNC41NzJBMTEuOTY1IDExLjk2NSAwIDAgMSAyNi4zNzYgMjJIMjNhMSAxIDAgMCAwLTEgMXYzLjM3N0ExMS45NTggMTEuOTU4IDAgMCAxIDQuNyAyMHoiIGZpbGw9ImN1cnJlbnRDb2xvciI+PC9wYXRoPjwvc3ZnPg=='
-
-const siteToUrl: Map<string, string> = new Map()
-
-otherUrls.forEach((url: string) => {
-  siteToUrl.set(url, 'https://0x3.com/icon?host=' + url)
-})
+export const DEFAULT_FAVICON =
+  'data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjMjg5M2ZkIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIzMiIgaGVpZ2h0PSIzMiI+PHBhdGggZD0iTTE2IDJhMTQgMTQgMCAxIDAgMCAyOGExNCAxNCAwIDAgMCAwLTI4em0wIDIuN2M0LjA3IDAgNy40NyAyLjMyIDkuMjMgNS43aC0xOC40NkM4LjUzIDcuMDIgMTEuOTMgNC43IDE2IDQuN3ptLTkuNjUgNy4zaDE5LjNjLjMyIDEuMDguNDggMi4yLjQ4IDMuM3MtLjE2IDIuMjItLjQ4IDMuM0g2LjM1YTUuODkgNS44OSAwIDAgMSAwLTYuNnptLjkyIDguM2gxNy41Yy0xLjc2IDMuMzgtNS4xNiA1LjctOS4yMyA1LjctNC4wNyAwLTcuNDctMi4zMi05LjIzLTUuN3oiLz48L3N2Zz4='
 
 function getDomainName(url: string) {
-  var domain = url.replace(/(^\w+:|^)\/\//, '')
-  //domain = domain.replace(/^www\./, '')//加上www.
-
-  var matches = domain.match(/([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)/)
-
-  if (matches && matches.length > 1) {
-    return matches[1]
-  }
-
-  return null
+  const domain = url.replace(/(^\w+:|^)\/\//, '')
+  const matches = domain.match(/([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)/)
+  return matches && matches.length > 1 ? matches[1] : null
 }
 
 export function getFaviconUrl(url: string) {
-  const paramsUrl = getDomainName(url)
-  if (paramsUrl == null) {
-    return ''
-  }
-
-  const optUrl = siteToUrl.get(paramsUrl)
-  if (optUrl) {
-    return optUrl
-  }
-  
-  return FAVICON_API + getDomainName(url) + '&size=64'
+  const domain = getDomainName(url)
+  if (!domain) return ''
+  const faviconUrl = `${FAVICON_API}${domain}&size=64`
+  console.log('[favicon]', faviconUrl)
+  return faviconUrl
 }
